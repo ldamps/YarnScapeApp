7e860ed14bbd234ee1b74604fd4fabac3b5da32c
Lirit Dampier <2560877@dundee.ac.uk>
2025-03-14 16:10:54 +0000

    my patterns in user profile

--- yarnscape-app/src/pages/Userprofile.tsx
+++ yarnscape-app/src/pages/Userprofile.tsx
@@ -2,18 +2,50 @@
 import { useNavigate } from 'react-router-dom';
 import {FaCog, FaArrowCircleLeft} from 'react-icons/fa';
 import './styles.css'
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import BottomNav from '../components/bottomNav';
+import { getAuth } from 'firebase/auth';
+import { db } from '../main';
+import { getFirestore, collection, query, where, getDocs, addDoc, updateDoc, doc } from 'firebase/firestore';
+
+interface Pattern {
+    id: string;
+    title: string;
+}
 
 const Userprofile = () => {
+    const auth = getAuth();
+    const user = auth.currentUser; // the current user
+    
+    const [myPatterns, setMyPatterns] = useState<Pattern[]>([]);
+    const [loading, setLoading] = useState(true);
 
     const navigate = useNavigate();
     const navigateToSettings = () => {
         navigate('/settings');
     }
-    const navigateToHome = () => {
-        navigate('/');
-    }
+    
+    // List the current user's patterns
+    useEffect(() => {
+        if (user) {
+            const fetchMyPatterns = async () => {
+                const q = query(collection(db, 'my-patterns'), where('userId', '==', user.uid));
+    
+                const querySnapshot = await getDocs(q);
+                const myPatternList: Pattern[] = [];
+                querySnapshot.forEach((doc) => {
+                    const data = doc.data();
+                    if (data.title) {
+                        myPatternList.push({ id: doc.id, title: data.title });
+                    }
+                });
+                setMyPatterns(myPatternList);
+                setLoading(false);
+            };
+    
+            fetchMyPatterns();
+        }
+    }, [db, user]);
 
     // For the bottom navbar
     const [currentTab, setCurrentTab] = useState('userprofile');
@@ -44,6 +76,32 @@ const Userprofile = () => {
 
                 <div className="my-patterns">
                     <h2>My Patterns: </h2>
+                    <div className="myPatterns-container">
+                        {loading ? (
+                            <p>Loading...</p>
+                        ) : (
+                            <div className='myPatterns-column'>
+                                {myPatterns.length > 0 ? (
+                                    <ul>
+                                        {myPatterns.map((pattern) => (
+                                            <li key={pattern.id}>
+                                                <div className="myPatterns-item">
+                                                    <span>{pattern.title}</span>
+                                                    <div className="myPatterns-columnbtns">
+                                                        <button>Edit</button>
+                                                        <button>Track</button>
+                                                        <button>Delete</button>
+                                                    </div>
+                                                </div>
+                                            </li>
+                                        ))}
+                                    </ul>
+                                ) : (
+                                    <p>no patterns</p>
+                                )}
+                            </div>
+                        )}
+                    </div>
                 </div>
 
                 <div className="saved-patterns">
